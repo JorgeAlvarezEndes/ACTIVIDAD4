d633158b3595678b81f9837e0979e6d4bbec375b
Jorge Alvarez Ceñal <jorgefpdam@gmail.com>
2022-04-23 11:38:34 +0200

    Version 2

--- AE4202221/src/Ejercicio.java
+++ AE4202221/src/Ejercicio.java
@@ -16,56 +16,80 @@ public class Ejercicio {
 		// Variables para el ejercicio
 		int num1 = 0;
 		int num2 = 0;
-		boolean valor1valido = false; // Para indicar que se cogio el numero correctamente
-		boolean valor2valido = false; // Para indicar que se cogio el numero correctamente
 		
 		// Mostramos el inicio del programa
 		System.out.println("Inicio del programa");
 		
-		// Cogemos el primer valor
-		while(valor1valido == false) {
-			Scanner teclado = new Scanner(System.in);	
-			System.out.println("introduce un valor para el numero 1:");
-			try {
-				num1 = teclado.nextInt();
-				if(num1 > 0) valor1valido = true;
-				else System.out.println("Valor no valido: ");
-			}
-			catch(Exception e) {
-				System.out.println("Valor no valido: " + e.getMessage());
-			}			
-		}
+		num1 = sacarNumero();
+		num2 = sacarNumero();
 		
-		// Cogemos el segundo valor
-		while(valor2valido == false) {
-			Scanner teclado = new Scanner(System.in);	
-			System.out.println("introduce un valor para el numero 2:");
-			try {
-				num2 = teclado.nextInt();
-				if(num2 > 0) valor2valido = true;
-				else System.out.println("Valor no valido: ");
-			}
-			catch(Exception e) {
-				System.out.println("Valor no valido: " + e.getMessage());
-			}			
-		}
-
 		// Mostramos que vamos a trabajar con ambos números
 		System.out.println("Vamos a trabajar con los numeros " + num1 + " y " + num2);
 	
 		// Realizamos las operaciones
-		int suma = num1 + num2;
-		int resta = num1 - num2;
-		int multiplicacion = num1 * num2;
-		float division = num1 / num2;
+		int suma = sumar(num1, num2);
+		int resta = restar(num1, num2);
+		int multiplicacion = multiplicar(num1, num2);
+		float division = dividir(num1, num2);
 		
 		// Mostramos en pantalla
+		mostrarResultados(num1, num2, suma, resta, multiplicacion, division);
+		
+		// Mostramos un mensaje de final de programa
+		System.out.println("Fin del programa");
+	}
+
+	// Metodo para mostrar los resultados en pantalla
+	private static void mostrarResultados(int num1, int num2, int suma, int resta, int multiplicacion, float division) {
 		System.out.println("la suma de " + num1 + " mas " + num2 + " es "  + suma);
 		System.out.println("la resta de " + num1 + " menos " + num2 + " es "  + resta);
 		System.out.println("la multiplicacion de " + num1 + " por " + num2 + " es "  + multiplicacion);
 		System.out.println("la division de " + num1 + " entre " + num2 + " es "  + division);
+	}
+
+	// Metodo para dividir dos numeros. Coge 2 enteros y devuelve un valor float
+	private static float dividir(int num1, int num2) {
+		float division = num1 / num2;
+		return division;
+	}
+
+	// Metodo para multiplicar 2 enteros y devuelve un entero con el resultado
+	private static int multiplicar(int num1, int num2) {
+		int multiplicacion = num1 * num2;
+		return multiplicacion;
+	}
+
+	// Metodo para restar 2 enteros y devuelve un entero con el resultado
+	private static int restar(int num1, int num2) {
+		int resta = num1 - num2;
+		return resta;
+	}
+
+	// Metodo para sumar 2 enteros y devuelve un entero con el resultado
+	private static int sumar(int num1, int num2) {
+		int suma = num1 + num2;
+		return suma;
+	}
+
+	// Metodo para pedir un numero entero positivo y devolverlo. En el caso de que sea
+	//  un valor no válido, seguirá pidiendo valores hasta obtener uno válido
+	private static int sacarNumero() {
+		// Cogemos el primer valor
+		boolean valido = false;
+		int num =0;
 		
-		// Mostramos un mensaje de final de programa
-		System.out.println("Fin del programa");
+		while(valido == false) {
+			Scanner teclado = new Scanner(System.in);	
+			System.out.println("introduce un valor para el numero 1:");
+			try {
+				num = teclado.nextInt();
+				if(num > 0) valido = true;
+				else System.out.println("Valor no valido: ");
+			}
+			catch(Exception e) {
+				System.out.println("Valor no valido: " + e.getMessage());
+			}			
+		}
+		return num;
 	}
 }

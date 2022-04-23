275a3264593677e517f3e7b1c34e54f9bff0b561
Jorge Alvarez Ceñal <jorgefpdam@gmail.com>
2022-04-23 10:05:19 +0200

    Version 1

--- AE4202221/src/Ejercicio.java
+++ AE4202221/src/Ejercicio.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
 
+/**
+ * Clase principal del programa. 
+ * Contiene el método main para hacerlo ejecutable
+ *  Es un ejercicio simple que pide dos numeros por consola y posteriormente
+ *  realiza las cuatro operaciones básicas con ellos: suma, resta, multiplicacion
+ *   y division
+ * 
+ * @author Jorge
+ * @version 1
+ * @since 23/04/2022
+ */
 public class Ejercicio {
 	public static void main(String[] args) {
+		// Variables para el ejercicio
 		int num1 = 0;
 		int num2 = 0;
-		boolean valor1valido = false;
-		boolean valor2valido = false;
+		boolean valor1valido = false; // Para indicar que se cogio el numero correctamente
+		boolean valor2valido = false; // Para indicar que se cogio el numero correctamente
 		
+		// Mostramos el inicio del programa
 		System.out.println("Inicio del programa");
+		
+		// Cogemos el primer valor
 		while(valor1valido == false) {
 			Scanner teclado = new Scanner(System.in);	
 			System.out.println("introduce un valor para el numero 1:");
@@ -21,6 +36,7 @@ public class Ejercicio {
 			}			
 		}
 		
+		// Cogemos el segundo valor
 		while(valor2valido == false) {
 			Scanner teclado = new Scanner(System.in);	
 			System.out.println("introduce un valor para el numero 2:");
@@ -34,19 +50,22 @@ public class Ejercicio {
 			}			
 		}
 
+		// Mostramos que vamos a trabajar con ambos números
 		System.out.println("Vamos a trabajar con los numeros " + num1 + " y " + num2);
 	
+		// Realizamos las operaciones
 		int suma = num1 + num2;
 		int resta = num1 - num2;
 		int multiplicacion = num1 * num2;
 		float division = num1 / num2;
 		
+		// Mostramos en pantalla
 		System.out.println("la suma de " + num1 + " mas " + num2 + " es "  + suma);
 		System.out.println("la resta de " + num1 + " menos " + num2 + " es "  + resta);
 		System.out.println("la multiplicacion de " + num1 + " por " + num2 + " es "  + multiplicacion);
 		System.out.println("la division de " + num1 + " entre " + num2 + " es "  + division);
 		
-		
+		// Mostramos un mensaje de final de programa
 		System.out.println("Fin del programa");
 	}
 }
